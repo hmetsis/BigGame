ce70fb91ba2e4ce74e4808a602274e1498a82a23
Elvira <elviramagnusson96@gmail.com>
2020-02-05 11:05:36 +0100

    metoder för liggande block

--- src/main/java/Block.java
+++ src/main/java/Block.java
@@ -48,4 +48,35 @@ public class  Block {
             terminal.putCharacter(blockChar);
         }
     }
+    public void lyingDown () {
+        int x = ThreadLocalRandom.current().nextInt(11, 59);
+        int y = 0;
+        int blockWidth = ThreadLocalRandom.current().nextInt(3, 10);
+        //blockArray = new Character[blockLength];
+
+        for (int i = 0; i < blockWidth; i++) {
+            Position position = new Position(x, y);
+            oneBlock.add(position);
+            x++;
+        }
+    }
+
+    public void moveLying (Terminal terminal) throws Exception{
+
+        for(Block b : oneBlock) {
+
+        }
+
+        int y = oneBlock.get(oneBlock.size()-1).getY();
+        int x = oneBlock.get(oneBlock.size()-1).getX();
+
+        oneBlock.add(new Position(x, y+1));
+        terminal.setCursorPosition(x, y+1);
+        terminal.putCharacter(blockChar);
+
+        terminal.setCursorPosition(oneBlock.get(0).getX(), oneBlock.get(0).getY());
+        terminal.putCharacter(' ');
+        oneBlock.remove(0);
+    }
+
 }
